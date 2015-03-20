1a7c788bd55128c9ba52537af21ffb1c681f3110
Alexandr Ovchinnikov <Alintorius@gmail.com>
2015-03-20 22:21:39 +0600

    Add custom SwitchCell and designed settings screen

---
+++ tauRSS/SettingsSwitchCell.h
@@ -0,0 +1,9 @@
+
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsSwitchCell : UITableViewCell
+@property (strong, nonatomic) IBOutlet UILabel *titleLabel;
+@property (strong, nonatomic) IBOutlet UISwitch *cellSwitch;
+
+@end
--- tauRSS/SettingsVC.m
+++ tauRSS/SettingsVC.m
@@ -1,13 +1,54 @@
 
 
 #import "SettingsVC.h"
+#import "SettingsSwitchCell.h"
+
+static NSString *const kSectionTitle = @"SectionTitle";
+static NSString *const kSectionItems = @"SectionItems";
+
+static NSString *const reuseIDswitchCell = @"SettingsSwitchCell";
+
+typedef NS_ENUM(NSInteger, Sections) {
+    sectionClear = 0,
+    sectionReading = 1,
+    sectionAbout = 2
+};
+
+typedef NS_ENUM(NSInteger, ReadingSettings) {
+    darkMode = 0,
+    fontSelect = 1
+};
 
 @interface SettingsVC () <UITableViewDataSource, UITableViewDelegate>
 @property (strong, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) NSArray *sections;
 
 @end
 
 @implementation SettingsVC
+
+- (instancetype)init
+{
+    if(self = [super init])
+    {
+        NSDictionary *sectionClear =
+            @{kSectionTitle: @"",
+              kSectionItems: @[@"Очистить кэш"]};
+        
+        NSDictionary *sectionReading =
+            @{kSectionTitle: @"Чтение",
+              kSectionItems: @[@"Ночной режим", @"Размер шрифта"]};
+        
+        NSDictionary *sectionAbout =
+        @{kSectionTitle: @"",
+          kSectionItems: @[@"О приложении"]};
+        
+        _sections = @[sectionClear, sectionReading, sectionAbout];
+        
+    }
+    return self;
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -18,7 +59,13 @@
                                               target:self
                                               action:@selector(didTouchDoneBarButtonItem:)];
         [self.navigationItem setRightBarButtonItem:doneBarButtonItem];
+        self.navigationItem.title = @"Настройки";
     }
+    
+    [self.tableView
+     registerNib:[UINib nibWithNibName:NSStringFromClass([SettingsSwitchCell class])
+                                bundle:[NSBundle mainBundle]]
+     forCellReuseIdentifier:reuseIDswitchCell];
     
 }
 
@@ -26,15 +73,45 @@
     [self.presentingViewController dismissViewControllerAnimated:YES completion:nil];
 }
 
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return [self.sections count];
+}
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 1;
+    NSDictionary *sectionDict = self.sections[section];
+    return ((NSArray *)sectionDict[kSectionItems]).count;
 }
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    NSDictionary *sectionDict = self.sections[section];
+    return sectionDict[kSectionTitle];
+}
+
+#define REUSABLE_CELL_ID @"ReuseID"
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [[UITableViewCell alloc]init];
-    return cell;
+    NSDictionary *section = self.sections[indexPath.section];
+    NSString *title = (section[kSectionItems])[indexPath.row];
+    
+    if ((indexPath.section == sectionReading) && (indexPath.row == darkMode))
+    {
+        SettingsSwitchCell *switchCell = [self.tableView dequeueReusableCellWithIdentifier:reuseIDswitchCell];
+        switchCell.titleLabel.text = title;
+        switchCell.cellSwitch.on = NO;
+        
+        return switchCell;
+    }
+    else
+    {
+        UITableViewCell *cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:REUSABLE_CELL_ID];
+        cell.textLabel.text = title;
+    
+        return cell;
+    }
 }
 
 
