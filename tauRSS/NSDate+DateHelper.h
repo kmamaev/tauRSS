bc0f93141c11921f773440834347d87588656aae
Konstantin Mamaev <kostya.plhs@gmail.com>
2015-03-21 17:55:50 +0600

    Add disign and toolbar to screen of article details

--- tauRSS/NSDate+DateHelper.h
+++ tauRSS/NSDate+DateHelper.h
@@ -3,6 +3,17 @@
 
 @interface NSDate (DateHelper)
 
-- (NSString *)convertToSpecialString;
+/**
+ *  Convert date to short format in accordance with condition if the day is today. If the day is
+ *  today returns only hours and minutes or day, month and year otherwise.
+ *  Examples: 12:51 (if today); 21.03.15  (if not today).
+ */
+- (NSString *)convertToShortString;
+
+/**
+ *  Convert date to string containing day, month, year, hours and minutes.
+ *  Example: 21.03.15, 12:51
+ */
+- (NSString *)convertToLongString;
 
 @end
