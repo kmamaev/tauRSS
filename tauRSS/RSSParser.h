c7d41b779b376b9626bd7760a8a759663761114c
Konstantin Mamaev <kostya.plhs@gmail.com>
2015-03-27 00:09:33 +0600

    Add source to parsed articles

--- tauRSS/RSSParser.h
+++ tauRSS/RSSParser.h
@@ -1,4 +1,5 @@
 #import <Foundation/Foundation.h>
+#import "Source.h"
 
 
 @interface RSSParser : NSObject
@@ -6,6 +7,6 @@
 /**
  *  Parses xml from the specified xml parser object and returns array of articles objects
  */
-- (NSMutableSet *)parseResponse:(NSXMLParser *)xmlParser;
+- (NSMutableSet *)parseResponse:(NSXMLParser *)xmlParser forSource:(Source *)source;
 
 @end
